a84d7b8629f89ac7358f8b4145abf727dbf460af
AbhijithNT <abhijithnt64@gmail.com>
2021-10-25 15:35:31 +0530

    ifsc

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import bot from "./bot/bot";
 import { command } from "./bot/commands";
+import { ifsc_ } from "./bot/utils";
 
 (async () => {
   await command();
+  await ifsc_();
   await bot.launch();
 })();
